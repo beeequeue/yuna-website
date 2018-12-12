acf78bf12f4175e2f9aff808d5d4591c5ce75a76
Adam <adam.lj.haglund@gmail.com>
2018-12-12 22:47:51 +0100

    add base for sections with lazy loaded images

--- src/js/index.ts
+++ src/js/index.ts
@@ -2,3 +2,28 @@ const el = document.querySelector('.below-fold')
 if (el) {
   el.innerHTML = 'loaded'
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const lazyImages = document.querySelectorAll('img.lazy')
+  console.log(lazyImages.length, ' images')
+
+  if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (!entry.isIntersecting) return
+
+        const image = entry.target as HTMLImageElement
+        image.src = image.dataset.src as string
+        image.srcset = image.dataset.srcset || ''
+
+        image.onload = () => {
+          image.classList.remove('lazy')
+          image.onload = null
+        }
+        observer.unobserve(image)
+      })
+    })
+
+    lazyImages.forEach(image => observer.observe(image))
+  }
+})
